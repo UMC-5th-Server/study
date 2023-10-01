dece8f63fbd5975a81b6e9257fd6077ab591d298
yeon015 <lizkitty@naver.com>
2023-10-01 17:09:29 +0900

    feat: 1주차 과제 업로드 #yeoni

---
+++ week1/module_test/constants.js
@@ -0,0 +1,5 @@
+// 변수를 사용한 예제
+module.exports = {
+  SEND_MAIN_PING: "send_main_ping",
+};
+// exports 했기에 SEND_MAIN_PING을 외부에서 사용할 수 있음.
---
+++ week1/module_test/module/minus.js
@@ -0,0 +1,5 @@
+const minus = function (a, b) {
+  return a - b;
+};
+
+module.exports = minus; // 얘 없으면 에러남
