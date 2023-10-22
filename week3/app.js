2fa9b35b28f625925a5c47adff388a060c201a12
박현지 <119161420+Park-Hyunji@users.noreply.github.com>
2023-10-22 17:50:17 +0900

    feat : 3주차 과제 업로드 #romit

---
+++ week3/app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var http = require('http');
+
+var app = express(); // express 객체
+
+app.set('port', process.env.PORT || 3000);
+
+var server = http.createServer(app).listen(app.get('port'),function(){
+    console.log('익스프레스로 웹 서버를 실행함 : ' + app.get('port'));
+    
+});
