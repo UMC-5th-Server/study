2b6d41d063e4bfd7f8d8b84dc1473b324f4a703a
박현지 <119161420+Park-Hyunji@users.noreply.github.com>
2023-11-03 17:23:16 +0900

    Add files via upload

---
+++ week6/test.ts
@@ -0,0 +1,23 @@
+class Test{
+    public static getTest(): string{
+        return "test";
+    }
+}
+
+Test.getTest();
+
+let n= 0;
+n = 'a';
+
+interface Person{
+    name: string;
+    age: number;
+}
+
+const p: Person = {
+    name:'mark',
+    age:35
+}
+
+Person[]
+Array<Person>
