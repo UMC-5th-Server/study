4d0b6fd9eff722454bd74976d3f97ca31d0c2d8e
aeeazip <codnjs0809@naver.com>
2023-11-05 22:41:19 +0900

    feat: 6주차 과제 업로드 #jay

---
+++ week06/TSExample/build/variableTest.js
@@ -0,0 +1,13 @@
+"use strict";
+var redeclare_var = "hi";
+var redclare_var = "hello";
+let a = "a";
+let b = "b";
+const c = "c";
+const d = "d";
+/*
+    1. a는 명시적으로 지정된 타입인 string
+    2. b는 타입추론에 의한 타입인 string
+    3. c는 명시적으로 지정된 타입인 string
+    4. d는 타입추론에 의한 리터럴타입
+*/
